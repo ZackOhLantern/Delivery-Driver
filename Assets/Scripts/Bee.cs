202a79edb5e9d7d4c2ccfdfdb7fcb50d41be1364
ZackOhLantern <92610630+ZackOhLantern@users.noreply.github.com>
2024-09-25 21:02:51 -0700

    public variables
    speed, steerspeed

--- Assets/Scripts/Bee.cs
+++ Assets/Scripts/Bee.cs
@@ -2,8 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Bee : MonoBehaviour
 {
+    public float speed = 0.1f;
+    public float steerSpeed = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +18,7 @@ public class Bee : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, 2f);   
-        transform.Translate(0,0.1f,0); 
+        transform.Rotate(0, 0, steerSpeed);   
+        transform.Translate(0,speed,0); 
     }
 }
