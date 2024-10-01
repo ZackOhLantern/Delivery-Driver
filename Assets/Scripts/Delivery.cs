91d4dcbb38641eadc5e3348acbb5f27bbb3dbf83
ZackOhLantern <92610630+ZackOhLantern@users.noreply.github.com>
2024-09-30 21:59:45 -0700

    onTrigger + IF statements

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -1,16 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Burst.Intrinsics;
 using UnityEngine;
 
-public class Collision : MonoBehaviour
+public class Delivery : MonoBehaviour
 {
     // Start is called before the first frame update
     void OnCollisionEnter2D(Collision2D other) {
-        Debug.Log("I'm poking you!");
+        Debug.Log("Ouch!");
     }
 
     void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("Trigger is happening!");
+        if (other.tag == "Package"){
+            Debug.Log("picked up Honey!");
+        }
+
+        if (other.tag == "Hive"){
+            Debug.Log("dropped off Honey!");
+        }
     }
+
 }
