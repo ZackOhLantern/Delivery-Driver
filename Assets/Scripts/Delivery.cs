9905daaf5546b6c6adbd6eaadc239cdc17423607
ZackOhLantern <92610630+ZackOhLantern@users.noreply.github.com>
2024-09-30 22:19:37 -0700

    Honey pickup and dropoff

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasHoney = false;
+
     // Start is called before the first frame update
     void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Ouch!");
@@ -14,10 +16,12 @@ public class Delivery : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Package"){
             Debug.Log("picked up Honey!");
+            hasHoney = true;
         }
 
-        if (other.tag == "Hive"){
+        if (other.tag == "Hive" && hasHoney == true){
             Debug.Log("dropped off Honey!");
+            hasHoney = false;
         }
     }
 
