1a7509c523c1437240bcb2d995737b6b8178aa44
ZackOhLantern <92610630+ZackOhLantern@users.noreply.github.com>
2024-09-30 23:03:17 -0700

    speed boosts + slow bumps

--- Assets/Scripts/Bee.cs
+++ Assets/Scripts/Bee.cs
@@ -6,21 +6,26 @@ using UnityEngine;
 
 public class Bee : MonoBehaviour
 {
-    [SerializeField] float speed = 15f;
+    [SerializeField] float speed = 5f;
     [SerializeField] float steerSpeed = 300f;
+    [SerializeField] float slowSpeed = 3f;
+    [SerializeField] float boostSpeed = 10f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
         float moveAmount = Input.GetAxis("Vertical") * speed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);   
         transform.Translate(0,moveAmount,0); 
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if (other.tag == "Boost"){
+            speed = boostSpeed;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other){
+        speed = slowSpeed;
     }
 }
